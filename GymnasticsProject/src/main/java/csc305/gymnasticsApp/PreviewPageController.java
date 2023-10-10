da862bc75a4eabb0b88c40aaa0cdac8cf5730a69
Johnny Breeden <johnnybreeden21@augustana.edu>
2023-10-10 11:22:14 -0500

    Fixed previewPage error

--- GymnasticsProject/src/main/java/csc305/gymnasticsApp/PreviewPageController.java
+++ GymnasticsProject/src/main/java/csc305/gymnasticsApp/PreviewPageController.java
@@ -1,4 +1,42 @@
 package csc305.gymnasticsApp;
 
+
+import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.Button;
+
 public class PreviewPageController {
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private Button printButton;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    void backButtonController(ActionEvent event) {
+        GymnasticsAppBeta.switchToMainEditDisplay();
+    }
+
+    @FXML
+    void homeButtonController(ActionEvent event) {
+        GymnasticsAppBeta.switchToHomePage();
+    }
+
+    @FXML
+    void printButtonController(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveController(ActionEvent event) {
+
+    }
+
 }
