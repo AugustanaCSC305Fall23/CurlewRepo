a58e770ed61c992571b656b7e3e61485c83a3a5e
Jack <jbigs6134@gmail.com>
2023-10-10 11:06:44 -0500

    Changed buttons to checkboxes, needs some design code

--- GymnasticsProject/src/main/java/csc305/gymnasticsApp/LessonPlanController.java
+++ GymnasticsProject/src/main/java/csc305/gymnasticsApp/LessonPlanController.java
@@ -3,6 +3,7 @@ package csc305.gymnasticsApp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 
 public class LessonPlanController {
 
@@ -11,9 +12,9 @@ public class LessonPlanController {
     private Button backButton;
     @FXML
     private Button doneButton;
-    @FXML private Button threeCardConfigButton;
-    @FXML private Button fourCardConfigButton;
-    @FXML private Button fiveCardConfigButton;
+    @FXML private CheckBox threeCPLCheckbox;
+    @FXML private CheckBox fourCPLCheckbox;
+    @FXML private CheckBox fiveCPLCheckbox;
 
     @FXML
     void backButtonHandle(ActionEvent event) {GymnasticsAppBeta.switchToHomePage();
@@ -23,22 +24,4 @@ public class LessonPlanController {
     void doneButtonHandle(ActionEvent event) {GymnasticsAppBeta.switchToMainEditDisplay();
     }
 
-    @FXML
-    void handleThreeButtonClick(ActionEvent event) {
-        threeCardConfigButton.setStyle("-fx-background-color: green;");
-        fourCardConfigButton.setStyle("-fx-background-color: red;");
-        fiveCardConfigButton.setStyle("-fx-background-color: red;");
-    }
-    @FXML
-    void handleFourButtonClick(ActionEvent event) {
-        threeCardConfigButton.setStyle("-fx-background-color: red;");
-        fourCardConfigButton.setStyle("-fx-background-color: green;");
-        fiveCardConfigButton.setStyle("-fx-background-color: red;");
-    }
-    @FXML
-    void handleFiveButtonClick(ActionEvent event) {
-        threeCardConfigButton.setStyle("-fx-background-color: red;");
-        fourCardConfigButton.setStyle("-fx-background-color: red;");
-        fiveCardConfigButton.setStyle("-fx-background-color: green;");
-    }
 }
