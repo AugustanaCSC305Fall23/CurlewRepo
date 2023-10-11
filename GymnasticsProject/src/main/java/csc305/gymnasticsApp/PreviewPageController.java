6875cdd6da578c862266d48da3c22abc283d5052
Johnny Breeden <johnnybreeden21@augustana.edu>
2023-10-11 10:20:53 -0500

    Fixed warning Alert and is now more user friendly

--- GymnasticsProject/src/main/java/csc305/gymnasticsApp/PreviewPageController.java
+++ GymnasticsProject/src/main/java/csc305/gymnasticsApp/PreviewPageController.java
@@ -5,6 +5,9 @@ import javafx.event.ActionEvent;
         import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
 
 public class PreviewPageController {
 
@@ -27,9 +30,24 @@ public class PreviewPageController {
 
     @FXML
     void homeButtonController(ActionEvent event) {
-        String message = new String("Are you sure you want to exit to the home page? Any unsaved changes will be lost.");
-        new Alert(Alert.AlertType.WARNING, message).showAndWait();
-        GymnasticsAppBeta.switchToHomePage();
+//        String message = new String("Are you sure you want to exit to the home page? Any unsaved changes will be lost.");
+//        new Alert(Alert.AlertType.WARNING, message).showAndWait();
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Caution");
+        alert.setHeaderText("Are you sure you want to exit to the home page? Any unsaved lessons will be lost.");
+        alert.setContentText("Select an option.");
+        ButtonType yesButton = new ButtonType("Yes");
+        ButtonType noButton = new ButtonType("No");
+        alert.getButtonTypes().setAll(yesButton, noButton);
+
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.get() == yesButton) {
+            GymnasticsAppBeta.switchToHomePage();
+        } else {
+            
+        }
+
     }
 
     @FXML
