0b8bf9075668cbe293a38d30bcf32310cd239551
braedenpoole <braedenpoole@gmail.com>
2023-10-13 23:17:53 -0500

    Reconfigured cards on mainEditDisplay to buttons, added some code to mainEditDisplayController to manage the new Go button I also added. Began some searchbar stuff but only partially began

--- GymnasticsProject/src/main/java/csc305/gymnasticsApp/MainEditDisplayController.java
+++ GymnasticsProject/src/main/java/csc305/gymnasticsApp/MainEditDisplayController.java
@@ -4,9 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
 public class MainEditDisplayController {
     @FXML
@@ -22,11 +24,30 @@ public class MainEditDisplayController {
     private ScrollBar scrollBar;
 
     @FXML
+    private Button drillSearchButton;
+
+    @FXML
+    private ScrollPane cardScrollPane;
+
+    @FXML
+    private VBox cardVBox;
+
+    @FXML
     void backButtonHandle(ActionEvent event) {GymnasticsAppBeta.switchToLessonPlan();
     }
 
     @FXML
     void previewButtonHandle(ActionEvent event) {GymnasticsAppBeta.switchToPreviewPage();
     }
+
+    @FXML
+    void goButtonHandle(ActionEvent event) {filterCards(drillSearchBar.getText().toLowerCase());
+    }
+
+
+    private void filterCards (String inputText){
+
+    }
+
 
 }
