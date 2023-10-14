0aba61d022767aa8b8fa215061a34d6d158d60ab
braedenpoole <braedenpoole@gmail.com>
2023-10-14 18:51:22 -0500

    mainEditDisplay search bar partially works, filtered visible buttons don't move to the top correctly though. Might need to change format of the scrollPane

--- GymnasticsProject/src/main/java/csc305/gymnasticsApp/MainEditDisplayController.java
+++ GymnasticsProject/src/main/java/csc305/gymnasticsApp/MainEditDisplayController.java
@@ -2,12 +2,14 @@ package csc305.gymnasticsApp;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class MainEditDisplayController {
@@ -33,6 +35,15 @@ public class MainEditDisplayController {
     private VBox cardVBox;
 
     @FXML
+    private HBox cardHBox1;
+
+    @FXML
+    private HBox cardHBox2;
+
+    @FXML
+    private HBox cardHBox3;
+
+    @FXML
     void backButtonHandle(ActionEvent event) {GymnasticsAppBeta.switchToLessonPlan();
     }
 
@@ -46,7 +57,22 @@ public class MainEditDisplayController {
 
 
     private void filterCards (String inputText){
-
+        for (Node hBoxNode : cardVBox.getChildren()) {
+            if (hBoxNode instanceof HBox) {
+                HBox hBox = (HBox) hBoxNode;
+                for (Node buttonNode : hBox.getChildren()) {
+                    if (buttonNode instanceof Button) {
+                        Button button = (Button) buttonNode;
+                        String buttonText = button.getId().toLowerCase();
+                        if (buttonText.contains(inputText)) {
+                            button.setVisible(true);
+                        } else {
+                            button.setVisible(false);
+                        }
+                    }
+                }
+            }
+        }
     }
 
 
