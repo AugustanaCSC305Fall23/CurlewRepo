fe2b97d9a2d6e4bc0441e1d47484156e73fc57bb
Johnny Breeden <johnnybreeden21@augustana.edu>
2023-10-26 12:02:20 -0500

    added a Course class

---
+++ GymnasticsProject/src/main/java/csc305/gymnasticsApp/Course.java
@@ -0,0 +1,17 @@
+package csc305.gymnasticsApp;
+
+import java.io.File;
+import java.util.List;
+
+public class Course {
+    private List<LessonPlan> plan;
+    private String courseTitle;
+
+    public void save(File saveFile) {
+
+    }
+
+    public static Course loadCourseFile(File courseFile) {
+        return null;
+    }
+}
