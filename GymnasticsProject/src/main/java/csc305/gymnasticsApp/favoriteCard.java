c5d619162286ef2e3aa4f058661c94dcbb41eb9c
Johnny Breeden <johnnybreeden21@augustana.edu>
2023-11-21 10:08:33 -0600

    created a subclass called favoiteCard to determine if a card has been favoited

---
+++ GymnasticsProject/src/main/java/csc305/gymnasticsApp/favoriteCard.java
@@ -0,0 +1,11 @@
+package csc305.gymnasticsApp;
+
+public class favoriteCard extends Card {
+
+    private boolean favorite;
+
+    public favoriteCard(boolean favorite) {
+        super();
+        this.favorite = favorite;
+    }
+}
