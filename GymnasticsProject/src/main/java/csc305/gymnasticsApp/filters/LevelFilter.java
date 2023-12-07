70ddc92b83caf8730cd9862edb40cf2d9abb65bc
Tyler Leslie <tylerleslie22@augustana.edu>
2023-12-07 11:27:10 -0600

    Finished JavaDoc comments in the Filters folder

--- GymnasticsProject/src/main/java/csc305/gymnasticsApp/filters/LevelFilter.java
+++ GymnasticsProject/src/main/java/csc305/gymnasticsApp/filters/LevelFilter.java
@@ -12,10 +12,21 @@ import java.util.List;
 public class LevelFilter implements CardFilter{
     private final String desiredLevel;
 
+    /**
+     * Constructs a LevelFilter with the specified desired level criteria
+     *
+     * @param desiredLevel The level criteria to fitler by
+     */
     public LevelFilter(String desiredLevel) {
         this.desiredLevel = desiredLevel;
     }
 
+    /**
+     * Checks if a Card object's level matches the desired level criteria
+     *
+     * @param card - The Card to be evaluated for a match.
+     * @return true if the card's level matches the desired criteria, false otherwise
+     */
     public boolean matches(Card card) {
         return desiredLevel.equalsIgnoreCase("All") || desiredLevel.toUpperCase().contains(card.getLevel().toUpperCase());
     }
