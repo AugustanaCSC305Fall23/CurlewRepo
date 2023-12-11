9d35094d32df60718f74dd0ff9434a5568d9c8bd
braedenpoole <braedenpoole@gmail.com>
2023-12-10 21:21:10 -0600

    Final Final 12/10, added Two JUnit tests

--- GymnasticsProject/src/test/java/csc305/gymnasticsApp/data/CardDatabaseTest.java
+++ GymnasticsProject/src/test/java/csc305/gymnasticsApp/data/CardDatabaseTest.java
@@ -3,14 +3,39 @@ package csc305.gymnasticsApp.data;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class CardDatabaseTest {
 
     @Test
-    void testLoadCards(){
-
+    void testNumLoadedCards(){
         CardDatabase.addCardsFromCSVFile();
+        List<Card> cardList = CardDatabase.getAllCards();
+        assertEquals(36, cardList.size());
+    }
 
+    @Test
+    void testEachCardLoadedValues(){
+        CardDatabase.addCardsFromCSVFile();
+        List<Card> cardList = CardDatabase.getAllCards();
+        for (Card card : cardList) {
+            assertNotNull(card);
+            assertNotNull(card.getImage());
+            assertNotNull(card.getUniqueID());
+            assertNotNull(card.getPackFolder());
+            assertNotNull(card.getCode());
+            assertNotNull(card.getTitle());
+            assertNotNull(card.getCode());
+            assertNotNull(card.getEquipment());
+            assertNotNull(card.getEvent());
+            assertNotNull(card.getGender());
+            assertNotNull(card.getLevel());
+            assertNotNull(card.getCategory());
+            assertNotNull(card.getModelGender());
+            assertNotNull(card.getKeywords());
+        }
     }
 }
