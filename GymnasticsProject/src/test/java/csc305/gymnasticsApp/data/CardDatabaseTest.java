ca871e393b7eb82968246054b1565b915f253461
Tyler Leslie <tylerleslie22@augustana.edu>
2023-12-10 19:09:29 -0600

    Renamed the application

--- GymnasticsProject/src/test/java/csc305/gymnasticsApp/data/CardDatabaseTest.java
+++ GymnasticsProject/src/test/java/csc305/gymnasticsApp/data/CardDatabaseTest.java
@@ -3,6 +3,8 @@ package csc305.gymnasticsApp.data;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CardDatabaseTest {
 
     @Test
